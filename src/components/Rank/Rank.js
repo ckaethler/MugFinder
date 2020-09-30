8d1f0b59eec882181a8af2d18ee9747802f91a2e
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-09-30 01:39:10 -0400

    added react particles

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className='white f3'>
+            {'Cat, your current rank is...'}
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+        
+    );
+}
+
+export default Rank;
