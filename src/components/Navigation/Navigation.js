2b82046737885147eb0979e4a9c04b030084fff5
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-03 13:26:24 -0400

    Updated SignIn form for API

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,34 @@
+// Top Navigation Component
 import React from 'react';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
+    // Handles if user is authenticated
     if(isSignedIn) {
         return(
+            // Creates navigation with only sign out function
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
                 <p 
                     onClick={() => onRouteChange('signout')}
-                    className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+                    className='f3 link dim black underline pa3 pointer'>
+                    Sign Out
+                </p>
             </nav>
         ); 
+    // Handles if user isn't authenticated
     } else {
         return ( 
+            // Creates nagivation with Sign In and Register routes
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
                 <p 
                     onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>SignIn</p>
+                    className='f3 link dim black underline pa3 pointer'>
+                    SignIn
+                </p>
                 <p 
                     onClick={() => onRouteChange('register')}
-                    className='f3 link dim black underline pa3 pointer'>Register</p>
+                    className='f3 link dim black underline pa3 pointer'>
+                    Register
+                </p>
             </nav>
         );
     }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -9,67 +9,99 @@ class SignIn extends React.Component {
         }
     }
 
+    // Sets signInEmail state
     onEmailChange = (event) => {
         this.setState({signInEmail: event.target.value})
     }
 
+    // Sets signInPassword state
     onPasswordChange = (event) => {
         this.setState({signInPassword: event.target.value})
     }
 
+    // Sends attempted email and password to API
     onSubmitSignIn = () => {
-        console.log(this.state);
+        fetch('http://localhost:3001/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data === 'success') {
+                    this.props.onRouteChange('home');
+                }
+            });
     }
 
     render() {
         const { onRouteChange } = this.props;
     
         return (
+            // Sign In Card
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 
                 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
+                        
+                        {/* Input Fields */}
                         <fieldset 
                             id="sign_up" 
                             className="ba b--transparent ph0 mh0">
-                        <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-                        <div className="mt3">
-                            <label 
-                                className="db fw6 lh-copy f6" 
-                                htmlFor="email-address">Email</label>
-                            <input 
-                                className="pa2 input-reset ba bg-transparent 
-                                    hover-bg-black hover-white w-100" 
-                                type="email" 
-                                name="email-address"  
-                                id="email-address" />
-                        </div>
-                        <div className="mv3">
-                            <label 
-                                className="db fw6 lh-copy f6" 
-                                htmlFor="password">Password</label>
-                            <input 
-                                className="b pa2 input-reset ba bg-transparent 
-                                    hover-bg-black hover-white w-100" 
-                                type="password" 
-                                name="password"  
-                                id="password" />
-                        </div>
+                            {/* Sign In Header */}
+                            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+
+                            {/* Email Field */}
+                            <div className="mt3">
+                                <label 
+                                    className="db fw6 lh-copy f6" 
+                                    htmlFor="email-address">Email</label>
+                                <input 
+                                    className="pa2 input-reset ba bg-transparent 
+                                        hover-bg-black hover-white w-100" 
+                                    type="email" 
+                                    name="email-address"  
+                                    id="email-address" 
+                                    onChange={this.onEmailChange} />
+                            </div>
+
+                            {/* Password Field */}
+                            <div className="mv3">
+                                <label 
+                                    className="db fw6 lh-copy f6" 
+                                    htmlFor="password">Password</label>
+                                <input 
+                                    className="b pa2 input-reset ba 
+                                        bg-transparent hover-bg-black 
+                                        hover-white w-100" 
+                                    type="password" 
+                                    name="password"  
+                                    id="password" 
+                                    onChange={this.onPasswordChange} />
+                            </div>
                         </fieldset>
+
+                        {/* Sign In Button */}
                         <div className="">
-                        <input 
-                            onClick={() => onRouteChange('home')}
-                            className="b ph3 pv2 input-reset ba b--black 
-                                bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Sign in" />
+                            <input 
+                                onClick={this.onSubmitSignIn}
+                                className="b ph3 pv2 input-reset ba b--black 
+                                    bg-transparent grow pointer f6 dib" 
+                                type="submit" 
+                                value="Sign in" />
                         </div>
+
+                        {/* Register Button */}
                         <div className="lh-copy mt3">
-                        <p 
-                            onClick={() => onRouteChange('register')} 
-                            className="f6 link dim black db pointer">
-                            Create Account
-                        </p>
+                            <p 
+                                onClick={() => onRouteChange('register')} 
+                                className="f6 link dim black db pointer">
+                                Create Account
+                            </p>
                         </div>
                     </div>
                 </main>
