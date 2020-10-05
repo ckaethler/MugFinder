b669976604142f0f449e79d239b7c13fa5d6ae42
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-04 23:41:11 -0400

    styling top navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 // Top Navigation Component
 import React from 'react';
 import './Navigation.css';
+import Logo from './Logo.png';
+import AppName from './AppName.png';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
     let navigation_links;
@@ -33,6 +35,21 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
     return(
         // Creates top navigation
         <nav className="top-nav">
+            {/* Logo and Name */}
+            <div className="full-logo pointer" onClick={() => onRouteChange('home')}>
+                <img 
+                    className="logo"
+                    src={Logo} 
+                    alt="Logo" 
+                    width="56px" 
+                    height="56px" />
+                <img
+                    className="app-name"
+                    src={AppName} 
+                    alt="MugFinder" />
+            </div>
+
+            {/* Right side nagivation links */}
             {navigation_links}
         </nav>
     ); 
--- src/controllers/App.js
+++ src/controllers/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Navigation from '../components/Navigation/Navigation';
-import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import Rank from '../components/Rank/Rank';
@@ -128,7 +127,6 @@ class App extends React.Component {
         {/* Authenticated: Main URl Detection Page */}
         { route === 'home' 
           ? <div>
-            <Logo />
             <Rank 
               firstName={firstName} 
               rank={rank} />
